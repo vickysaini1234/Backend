5cb4dc87be86c9d297f271ee02ceb9facb846553
vikki kumar <vickysaini73022@gmail.com>
2026-02-23 19:43:16 +0530

    add multer in food routes for file uploader

--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -2,9 +2,13 @@ const express=require("express")
 const foodController=require("../controllers/food.controllers")
 const authMiddleware=require("../middlewares/auth.middleware")
 const router=express.Router()
+const multer=require("multer")
+const upload=multer({
+    storage:multer.memoryStorage(),
+})
 
 // post/api/food/ [protected]
 
-router.post('/',authMiddleware.authFoodPartnerMiddleware,foodController.createFood)
+router.post('/',authMiddleware.authFoodPartnerMiddleware,upload.single("video"),foodController.createFood)
 
 module.exports=router
