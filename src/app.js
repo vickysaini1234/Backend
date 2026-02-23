91e5007e3bb46e61199c851906c70a056fd4c942
vikki kumar <vickysaini73022@gmail.com>
2026-02-23 15:50:52 +0530

    create food api app.js file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const cookieParser=require("cookie-parser")
 const authRoutes=require("./routes/auth.routes")
+const foodRoutes=require("./routes/food.routes")
 
 
 
@@ -13,6 +14,9 @@ app.get("/",(req,res)=>{
 })
 
 app.use('/api/auth',authRoutes)
+
+
+app.use('/api/food',foodRoutes)
 
 
 module.exports=app
