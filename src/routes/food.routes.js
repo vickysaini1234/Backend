fe3b3922176fc8ddd1f42a118bd718d7a069c477
vikki kumar <vickysaini73022@gmail.com>
2026-02-23 19:01:32 +0530

    add middleware in food routes path

--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -1,9 +1,10 @@
 const express=require("express")
-
+const foodController=require("../controllers/food.controllers")
+const authMiddleware=require("../middlewares/auth.middleware")
 const router=express.Router()
 
 // post/api/food/ [protected]
 
-router.post('/')
+router.post('/',authMiddleware.authFoodPartnerMiddleware,foodController.createFood)
 
 module.exports=router
