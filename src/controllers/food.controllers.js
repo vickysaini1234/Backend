c58b72108942feee861b6222eb41c5a077e2ef0b
vikki kumar <vickysaini73022@gmail.com>
2026-02-23 19:45:03 +0530

    add package

--- src/controllers/food.controllers.js
+++ src/controllers/food.controllers.js
@@ -2,6 +2,12 @@ const foodModel=require("../models/food.model")
 
 async function createFood(req,res){
 
+    console.log(req.foodPartner);
+    console.log(req.body);
+    console.log(req.file);
+    
+    res.send("food item added")
+
 }
 
 module.exports={
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt =require("jsonwebtoken")
 
 
 async function authFoodPartnerMiddleware(req,res,next){
-    const token=req.cookie.token;
+    const token=req.cookies.token;
 
     if(!token){
         return res.status(401).json({
